90f3ae3cc7b1341399193846c210aa1a6227ce4f
edwinws <william.edwinx@gmail.com>
2022-01-16 22:57:42 +0800

    #1 Restructured main program into its own folder

---
+++ MainService/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MainService;
+class Program {
+    static void Main(string[] args) {
+        var prog = new Program();
+        // prog.displaySplashArt();
+        prog.displayMenu();
+
+        var option = prog.getOptionInput();
+        prog.getOptionInstance(option).Run();
+    }
+
+    private void displayMenu() {
+        Console.WriteLine("Menu goes here");
+    }
+
+    private int getOptionInput() {
+        return 0;
+    }
+
+
+    private IConsoleOption getOptionInstance(int option) {
+        string[] classMapping = {
+            "FileReadWrite1",
+        };
+
+        Type? type = Type.GetType(classMapping[option]);
+        var instance = (IConsoleOption?)Activator.CreateInstance(type!);
+
+        return instance!;
+    }
+}
